789efa1378a7924ab6e6148d946404a3735c7401
Voidyy <96418649+SaczRise@users.noreply.github.com>
2023-06-13 20:45:24 +0700

    E_Bot.py
    Main Script

--- E_Bot.py
+++ E_Bot.py
@@ -4,24 +4,24 @@ import discord
 from discord.ext import commands
 import random, os
 
-#Intensts
+# Intensts
 intents = discord.Intents.default()
 intents.message_content = True
 
-#Bot variables
+# Bot variables
 bot = commands.Bot(command_prefix='$', intents=intents)
 
-#Bot commands
-
-@bot.event                         #Bot will know if something happened
-async def on_ready():              #If bot is ready
+# Bot commands
+@bot.event                                                                            # Bot will know if something happened
+async def on_ready():                                                                 # If bot is ready
     print('Bot ready!')
 
-@bot.command()                     #Command that will command for bot
+@bot.command()                                                                        # Command that will command for bot
 async def handwork(ctx):
     img_name = random.choice(os.listdir(handwork))
-    with open(f'Handworkd/{img_name}', 'rb') as image:      # Open image and keep it in variable
+    with open(f'Handworkd/{img_name}', 'rb') as image:                                # Open image and keep it in variable
         picture = discord.file(image)
-    await ctx.send(picture)
+    await ctx.send(file=picture)
+
 
 bot.run('Your Token') # Bot Token
